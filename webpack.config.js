ce75874e5c7e37075f327dcc41fb0d562e9558b4
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-11 17:50:35 +0500

    Change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV === 'development'
 
 module.exports = {
   mode: NODE_ENV ? NODE_ENV : 'development',
-  entry: path.resolve(__dirname, './src/index.jsx'),
+  entry: path.resolve(__dirname, './src/index.tsx'),
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'index.bundle.js'
+    filename: 'index.js'
   },
 
   devServer: {
     port: 3000,
-    watchContentBase: true
+    open: true,
+    hot: IS_DEV
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {loader: 'babel-loader'}
+        use: {
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: {loader: 'ts-loader'}
+        use: [
+          'babel-loader',
+				  'ts-loader'
+        ]
       },
       {
         test: /\.(scss|css)$/,
@@ -35,5 +43,16 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html')
+    })
+  ]
 }
