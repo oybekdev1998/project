dbb171fa824fb13b2e8021504ca2faba42c6a009
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-28 08:54:44 +0500

    Created and styled header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,15 @@
+/* eslint-disable react/no-children-prop */
 import React from 'react'
+import Button from '../UI/Button/Button'
 import styles from './Header.module.scss'
+
 
 const Header: React.FC = () => {
   return (
-    <div className={styles.header}>Header</div>
+    <div className={styles.header}>
+      <Button style={{color: '#a3daa5', border: '1px solid rgba(163,218,165, .5)'}} children="Создать резюме" />
+      <Button style={{color: '#fff', border: '1px solid hsla(0,0%,100%,.5)'}} children='Войти' />
+    </div>
   )
 }
 
