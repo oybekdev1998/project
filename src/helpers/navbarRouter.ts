69d5fd020e4ae7a3830ac1fdc3b7126cf247943e
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-28 08:55:47 +0500

    Created navbarRouter

--- src/helpers/navbarRouter.ts
+++ src/helpers/navbarRouter.ts
@@ -1,6 +1,6 @@
 export const navbarRouter = [
-  { url: '/', name: 'Россия'},
-  { url: '/applicant', name: 'Соискателям'},
+  { url: '/search', name: 'Россия'},
+  { url: '/', name: 'Соискателям'},
   { url: '/employer', name: 'Работодателям'},
   { url: '/finishedResume', name: 'Готовой резюме'},
   { url: '/interview', name: 'Репетиция собеседования'},
